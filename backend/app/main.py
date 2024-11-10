eae760d2056967bc1dab9056aa3fda2feedfa82d
josephlbj <josephlbj@users.noreply.github.com>
2024-11-10 02:00:13 +0000

    Added database and connection to the geoip database

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 from app.core.database import create_db_and_tables, SessionDep
+from app.core.geoip import get_geo_info
 
 app = FastAPI()
 create_db_and_tables()
@@ -15,3 +16,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/ip_info/{ip}")
+def ip_info(ip: str, session: SessionDep):
+    return {"response": get_geo_info(ip, session)}
