0ee1b78d953480e44dbf3457462d4fd082516125
micraj <mrajzer2004@gmail.com>
2024-11-10 06:49:05 +0000

    Fixed fetch

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -12,7 +12,7 @@
   let marker2Position = 1;
   let loading = false;
   let error;
-  let data;
+  let response;
 
   // Get current date and time
   let currentDateTime = new Date();
@@ -30,29 +30,40 @@
       // Set loading to true while fetching data
       loading = true;
       // Replace with your API endpoint
-      const response = await fetch(
-        `http://127.0.0.1:8000/data?starttime=${epochTime1}&endtime=${epochTime2}/`
+      const webresponse = await fetch(
+        `http://127.0.0.1:8000/data?starttime=${epochTime1}&endtime=${epochTime2}`,
       ); // TODO ROUTE
 
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
+      if (!webresponse.ok) {
+        throw new Error(`Error: ${webresponse.status}`);
       }
 
       // Parse the JSON response
       // Extract latitude and longitude from each data item and store in arcsData
-      console.log(latitude, longitude);
-      arcsData = response.map((item) => ({
-        startLat: latitude,
-        startLng: longitude,
-        endLat: item.latitude,
-        endLng: item.longitude,
-        color: "red",
-      }));
+
+      let response = await webresponse.json();
+      console.log(response);
+      arcsData = [];
+      for (let i = 0; i < response.length; i++) {
+        let item = response[i];
+        console.log(item);
+        if (item.latitude == "" || item.longitude == "") {
+          continue;
+        }
+        arcsData.push({
+          startLat: latitude,
+          startLng: longitude,
+          endLat: item.latitude,
+          endLng: item.longitude,
+          color: "red",
+        });
+      }
       console.log(arcsData);
       // Update the earth arcs data
     } catch (err) {
       error = err.message;
       console.log(error);
+      console.log(response);
     } finally {
       loading = false;
     }
@@ -85,7 +96,7 @@
               errorMessage = "An unknown error occurred.";
               break;
           }
-        }
+        },
       );
     } else {
       errorMessage = "Geolocation is not supported by this browser.";
