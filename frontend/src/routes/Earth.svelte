f4a77a250ff00a5125c751616e24e2d09afc9256
Saud <whydidyoumakethis@gmail.com>
2024-11-10 00:11:25 +0000

    made nice temp graph thingy

--- frontend/src/routes/Earth.svelte
+++ frontend/src/routes/Earth.svelte
@@ -8,8 +8,8 @@ onMount(async() => {
         // Gen random data
         const N = 20;
         const arcsData = [...Array(N).keys()].map(() => ({
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
+        startLat: 53.484824,
+        startLng: -2.240126,
         endLat: (Math.random() - 0.5) * 180,
         endLng: (Math.random() - 0.5) * 360,
         color: [['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]]
@@ -45,16 +45,40 @@ onMount(async() => {
         z-index: 1;
     }
     div.data{
+        display: flex;
+        flex-wrap: wrap;
         z-index: 2;
+    }
+    div.data h1{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
     }
     div.border{
         position: absolute;
         width: 432px;
         top: 240px;
         left: 108px;
-        border: 1px solid black;
         z-index: 3;
         background-color: white;
+    }
+    div.title{
+        width: 100%;
+        height: 50px;
+        margin: 5px;
+        padding: 10px;
+        border: 1px solid black;
+
+    }
+    div.graphs{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    div.graphs h1{  
+        border: 1px solid black;
+        padding: 40px;
     }
 
 
@@ -62,7 +86,11 @@ onMount(async() => {
 <div class="position">
     <div class="border">
     <div class="data">
+        <div class="title">
         <h1>dadffdadas</h1>
+    </div>
+       <div class="graphs"><h1> graph1</h1> <h1> graph2</h1><h1> graph3</h1><h1> graph4</h1>
+    </div> 
     </div>        
     </div>
 
