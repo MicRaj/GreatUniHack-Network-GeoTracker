23b08ffa8e6c7c045101b47e82848852a6629237
micraj <mrajzer2004@gmail.com>
2024-11-10 01:08:04 +0000

    Add CORS support and enhance Timeline component with data fetching

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,29 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allowing CORS from the specific origin (localhost:5173) or any origin
+origins = [
+    "http://localhost:5173",  # Frontend app URL
+    "http://127.0.0.1:5173",  # Or you can use localhost and 127.0.0.1 as the origin
+]
+
+# Adding CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins or "*" for all origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # List allowed methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/")
+def read_item():
+    return {"message":"Hello"}
--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -5,10 +5,40 @@
 
   let marker1Position = 0;
   let marker2Position = 1;
+  let loading = false;
+  let error;
+  let data;
 
+  // Function to fetch data from an API
+  async function fetchData() {
+    try {
+      // Set loading to true while fetching data
+      loading = true;
+      // Replace with your API endpoint
+      const response = await fetch("http://127.0.0.1:8000/items/"); // TODO ROUTE
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      // Parse the JSON response
+      data = await response.json();
+    } catch (err) {
+      error = err.message;
+      console.log(error);
+    } finally {
+      loading = false;
+    }
+  }
 </script>
+
 <Navbar />
-<Timeline bind:normPos1={marker1Position} bind:normPos2={marker2Position} timelineWidth=1000/>
+<Timeline
+  bind:normPos1={marker1Position}
+  bind:normPos2={marker2Position}
+  timelineWidth="1000"
+  sendGetRequest={fetchData}
+/>
 <div>
   <p>Marker 1 Position: {marker1Position}</p>
   <p>Marker 2 Position: {marker2Position}</p>
